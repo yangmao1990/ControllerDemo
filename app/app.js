9b56da1c2697f75d7dabf016902feb179fdcf50b
yangmao1990 <yangmao@buffalo.edu>
2015-05-22 02:25:48 -0400

    A small example on Angular Controller

---
+++ app/app.js
@@ -0,0 +1,34 @@
+﻿var myApp = angular.module('inventoryApp', []);
+
+myApp.controller('InventoryController', function ($scope) {
+
+    $scope.message = 'Hello...Angular';
+
+    $scope.sayHello = function () {
+        alert('Hello...from Angular controller');
+    }
+
+    function init() {
+        alert('Controller Initialized');
+    }
+
+    init();
+
+});
+
+
+
+
+var myWebAdminApp = angular.module('myWebAdminApp', []);
+myWebAdminApp.controller('IndexController', function ($scope) {
+
+    $scope.appName = 'Web Administrtion Tool';
+
+    $scope.greet = function () {
+        alert('Hello..'+$scope.name);
+    }
+});
+
+
+
+
